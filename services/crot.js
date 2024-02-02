7ac5f1d1189d97b2a9b86c53e78950d9d9134753
Imanuel Urip Subekti <imanuel.subekti@efishery.com>
2024-02-02 18:32:57 +0700

    chore: move to env

--- services/crot.js
+++ services/crot.js
@@ -37,12 +37,13 @@ const getQueries = async () => {
 
 const CrotService = {
     getLinks: async () => {
+        const baseUrl = process.env.DOODSTREAM_PRO_URL
         const queries = await getQueries();
         const query = queries[Math.floor(Math.random() * queries.length)];
-        const res = await Axios.get(`https://doodstream.pro/search/?q=${query}`);
+        const res = await Axios.get(`${baseUrl}/search/?q=${query}`);
         const regex = /https:\/\/doodstream\.pro\/\/video\/(\w+)/g;
         let matches = ((res.data || "").match(regex) || [])
-            .map((s) => s.replace("https://doodstream.pro//video/", "https://doods.pro/e/"));
+            .map((s) => s.replace(`${baseUrl}//video/`, `https://doods.pro/e/`));
 
         if (matches.length > 0) {
             matches.map((m) => {
