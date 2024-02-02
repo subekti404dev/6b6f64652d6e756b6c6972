5ffa3fe0de6ff49c8337e611cbccc61c75121e92
Imanuel Urip Subekti <imanuel.subekti@efishery.com>
2024-02-02 11:27:04 +0700

    fix: build

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { useTheme } from "next-themes";
 import { useCrot } from "../hooks/useCrot";
 import { useEffect } from "react";
 import { BallTriangle } from "react-loader-spinner";
-import { Toaster, toast } from "react-hot-toast"
+import { Toaster, toast } from "react-hot-toast";
+import Image from "next/image";
 
 const Index = () => {
   const { setTheme } = useTheme();
@@ -27,13 +28,13 @@ const Index = () => {
                     toast.success("copied");
                   }}
                 >
-                  <img src="copy.png" tw="width[18px]" />
+                  <Image src="/copy.png" width={16} height={18} />
                 </button>
               </div>
             </div>}
             {url && key && (
               <div
-                tw="text-center background-color[#3a3a3a] m-4 p-1 border-radius[0.5rem] word-wrap[break-word] font-family[monospace]">
+                tw="text-center background-color[#3a3a3a] m-4 p-2 border-radius[0.5rem] word-wrap[break-word] font-family[monospace]">
                 <div tw="flex flex-row">
                   <div tw="width[90%]"> {key}</div>
                   <button
@@ -43,7 +44,7 @@ const Index = () => {
                       toast.success("copied");
                     }}
                   >
-                    <img src="copy.png" tw="width[18px]" />
+                    <Image src="/copy.png" width={16} height={18} />
                   </button>
                 </div>
               </div>
