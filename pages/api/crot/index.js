6bc3b49503cefb21621bde53a5e1087afecc42ab
Imanuel Urip Subekti <imanuel.subekti@efishery.com>
2024-02-03 18:15:51 +0700

    disable twit

--- pages/api/crot/index.js
+++ pages/api/crot/index.js
@@ -1,12 +1,18 @@
 import { encryptWahyuWay } from "../../../utils/enc";
 import CrotService from "../../../services/crot";
-import TwitterService from "../../../services/twitter";
+// import TwitterService from "../../../services/twitter";
 
 export default async function handler(req, res) {
     try {
         const key = req.query.key || "bagiBwang";
-        const [link1, link2] = await Promise.all([CrotService.getRandomLink(), TwitterService.getRandomLink()]);
-        if (!link1 || !link2) throw new Error("failed to get link");
+        const [
+            link1,
+            // link2
+        ] = await Promise.all([
+            CrotService.getRandomLink(),
+            // TwitterService.getRandomLink()
+        ]);
+        if (!link1) throw new Error("failed to get link");
         const link = `${link1}\n${link2}`;
         const encLink = await encryptWahyuWay(link, key);
         if (!encLink) throw new Error("failed to encrypt link");
